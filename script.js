df491222e955934940e1fd7d7c7fe6b795690d30
Luka <59731107+LukaSmiljanic@users.noreply.github.com>
2020-01-12 01:46:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches(".dropbtn")) {
+    var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains("show")) {
+      myDropdown.classList.remove("show");
+    }
+  }
+};
+
+// Sticky header on scroll
+window.onscroll = function() {
+  stickyHeader();
+};
+
+var header = document.getElementById("navbar");
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
